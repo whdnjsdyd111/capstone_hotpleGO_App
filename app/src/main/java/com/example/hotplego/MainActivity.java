c02380920194ecae4cec40333eb8ad965b0f7962
whdnjsdyd111 <66655578+whdnjsdyd111@users.noreply.github.com>
2021-03-17 11:11:59 +0900

    bottom navigation 다섯가지 추가

--- app/src/main/java/com/example/hotplego/MainActivity.java
+++ app/src/main/java/com/example/hotplego/MainActivity.java
@@ -1,8 +1,14 @@
 package com.example.hotplego;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,8 +16,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        
-        // 조원용
-        // 이주영 1
+        BottomNavigationView navView = findViewById(R.id.nav_view);
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications,
+                R.id.navigation_search, R.id.navigation_myPage)
+                .build();
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+        NavigationUI.setupWithNavController(navView, navController);
     }
+
 }
---
+++ app/src/main/java/com/example/hotplego/ui/mypage/MyPageViewModel.java
@@ -0,0 +1,19 @@
+package com.example.hotplego.ui.mypage;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class MyPageViewModel extends ViewModel {
+
+    private MutableLiveData<String> mText;
+
+    public MyPageViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is myPage fragment");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
