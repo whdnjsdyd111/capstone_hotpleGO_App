8d254a67bf4e18992809ff02dbddff56e84f2c5a
Park <baallon@g.yju.ac.kr>
2021-04-05 20:31:36 +0900

    사용자 회원가입 / 로그인 / 비밀번호 찾기 레이아웃 drawable 이미지 추가 values 디렉토리 string.xml 추가작업

--- app/src/main/java/com/example/hotplego/MainActivityLogin.java
+++ app/src/main/java/com/example/hotplego/MainActivityLogin.java
@@ -1,4 +1,14 @@
 package com.example.hotplego;
 
-public class MainActivityLogin {
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivityLogin extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+
+    }
 }
