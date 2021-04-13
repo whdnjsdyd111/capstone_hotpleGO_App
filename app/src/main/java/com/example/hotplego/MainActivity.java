8381f2f03d3090bab959a94ad9c531ed39e74825
jh675 <kerl6632@gmail.com>
2021-04-13 11:51:44 +0900

    레이아웃 및 카드뷰 재작성

--- app/src/main/java/com/example/hotplego/MainActivity.java
+++ app/src/main/java/com/example/hotplego/MainActivity.java
@@ -1,18 +1,29 @@
 package com.example.hotplego;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-import java.util.ArrayList;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button button_notice;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button_notice = findViewById(R.id.button12);
+
+        button_notice.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), Order_list.class);
+                startActivity(intent);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/hotplego/Order_details_New.java
+++ app/src/main/java/com/example/hotplego/Order_details_New.java
@@ -1,6 +1,29 @@
 package com.example.hotplego;
 
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 public class Order_details_New extends Fragment {
+    private View view;
+
+    public static Order_details_New newInstance() {
+        Order_details_New order_details_new = new Order_details_New();
+
+        return order_details_new;
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @NonNull ViewGroup viewGroup, @Nullable Bundle savedInstanceState) {
+        view = inflater.inflate(R.layout.fragment_order_details_new, viewGroup,false);
+
+        return view;
+    }
 }
