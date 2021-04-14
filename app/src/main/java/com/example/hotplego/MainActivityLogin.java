90f939994cfeab3677c2f524553ed284bfb2175b
jh675 <kerl6632@gmail.com>
2021-04-14 10:20:04 +0900

    로그인 작업

--- app/src/main/java/com/example/hotplego/MainActivityLogin.java
+++ app/src/main/java/com/example/hotplego/MainActivityLogin.java
@@ -1,11 +1,13 @@
 package com.example.hotplego;
 
-
+import android.Manifest;
 import android.app.Activity;
 import android.content.Intent;
-import android.content.SharedPreferences;
-import android.database.Cursor;
+import android.content.pm.PackageManager;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.StrictMode;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
@@ -13,6 +15,19 @@ import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import static com.example.hotplego.R.layout;
 
 public class MainActivityLogin extends Activity implements View.OnClickListener {
     Spinner spinner;
@@ -22,16 +37,11 @@ public class MainActivityLogin extends Activity implements View.OnClickListener 
     String id, pw, Logout_Code;
     CheckBox checkBox;
     Intent intent;
-    Cursor cursor;
-    SharedPreferences autoLogin;
-    SharedPreferences.Editor editor;
-
-    public static String sloginId;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(layout.activity_login);
         loginId = (EditText) findViewById(R.id.etEmail);
         loginPw = (EditText) findViewById(R.id.etPassword);
         btnLogin = (Button) findViewById(R.id.btnLogin);
@@ -42,6 +52,12 @@ public class MainActivityLogin extends Activity implements View.OnClickListener 
         btnSignup.setOnClickListener(this);
         btnSearch.setOnClickListener(this);
 
+        checkDangerousPermissions();
+        if (android.os.Build.VERSION.SDK_INT > 9) {
+            StrictMode.ThreadPolicy policy = new
+                    StrictMode.ThreadPolicy.Builder().permitAll().build();
+            StrictMode.setThreadPolicy(policy);
+        }
     }
 
     @Override
@@ -52,11 +68,23 @@ public class MainActivityLogin extends Activity implements View.OnClickListener 
                 id = loginId.getText().toString();
                 pw = loginPw.getText().toString();
 
-                if(id.length() == 0 || pw.length()== 0) { // 아이디 비번 입력 X
+                if (id.length() == 0 || pw.length() == 0) { // 아이디 비번 입력 X
                     Toast.makeText(this, "아이디 또는 비밀번호를 입력하세요!", Toast.LENGTH_SHORT).show();
                     return;
                 }
-               break;
+                Log.w("login", "로그인 하는중");
+                try {
+
+                    Log.w("앱에서 보낸값", id + ", " + pw);
+                    CustomTask task = new CustomTask();
+                    String result = task.execute(id, pw).get();
+                    Log.w("받은값", result);
+
+                } catch (Exception e) {
+
+                }
+                break;
+
 
             case R.id.tvRegister: // 회원가입 버튼
                 intent = new Intent(getApplicationContext(), SelectRegister.class);
@@ -67,6 +95,96 @@ public class MainActivityLogin extends Activity implements View.OnClickListener 
                 intent = new Intent(getApplicationContext(), MainActivitySearch.class);
                 startActivity(intent);
                 break;
+        }
+
+
+    }
+    private void checkDangerousPermissions() {
+        String[] permissions = {
+                Manifest.permission.INTERNET,
+                Manifest.permission.ACCESS_NETWORK_STATE,
+                Manifest.permission.ACCESS_WIFI_STATE
+        };
+
+        int permissionCheck = PackageManager.PERMISSION_GRANTED;
+        for (int i = 0; i < permissions.length; i++) {
+            permissionCheck = ContextCompat.checkSelfPermission(this, permissions[i]);
+            if (permissionCheck == PackageManager.PERMISSION_DENIED) {
+                break;
+            }
+        }
+
+        if (permissionCheck == PackageManager.PERMISSION_GRANTED) {
+            Toast.makeText(this, "권한 있음", Toast.LENGTH_LONG).show();
+        } else {
+            Toast.makeText(this, "권한 없음", Toast.LENGTH_LONG).show();
+
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this, permissions[0])) {
+                Toast.makeText(this, "권한 설명 필요함.", Toast.LENGTH_LONG).show();
+            } else {
+                ActivityCompat.requestPermissions(this, permissions, 1);
+            }
+        }
+    }
+
+    class CustomTask extends AsyncTask<String, Void, String> {
+        String sendMsg, receiveMsg;
+        @Override
+        // doInBackground의 매개변수 값이 여러개일 경우를 위해 배열로
+        protected String doInBackground(String... strings) {
+            try {
+                String str;
+                URL url = new URL("http://192.168.1.34:8000/android/login");  // 어떤 서버에 요청할지(localhost 안됨.)
+                // ex) http://123.456.789.10:8080/hello/android
+                HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+                conn.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+                conn.setRequestMethod("POST");                              //데이터를 POST 방식으로 전송합니다.
+                conn.setDoOutput(true);
+                Log.i("conn", "URL 완료");
+
+                // 서버에 보낼 값 포함해 요청함.
+                OutputStreamWriter osw = new OutputStreamWriter(conn.getOutputStream());
+                sendMsg = "uCode="+strings[0]+"&pw="+strings[1]; // GET방식으로 작성해 POST로 보냄 ex) "id=admin&pwd=1234";
+                osw.write(sendMsg);                           // OutputStreamWriter에 담아 전송
+                osw.flush();
+                osw.close();
+                Log.i("conn", "값 전송 완료");
+
+                // jsp와 통신이 잘 되고, 서버에서 보낸 값 받음.
+                if(conn.getResponseCode() == conn.HTTP_OK) {
+                    InputStreamReader tmp = new InputStreamReader(conn.getInputStream(), "UTF-8");
+                    BufferedReader reader = new BufferedReader(tmp);
+                    StringBuffer buffer = new StringBuffer();
+                    while ((str = reader.readLine()) != null) {
+                        buffer.append(str);
+                    }
+                    receiveMsg = buffer.toString();
+                } else {    // 통신이 실패한 이유를 찍기위한 로그
+                    Log.i("통신 결과", conn.getResponseCode()+"에러");
+                }
+
+                Log.i("통신 결과", "통신완료");
+
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            // 서버에서 보낸 값을 리턴합니다.
+            return receiveMsg;
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        if (requestCode == 1) {
+            for (int i = 0; i < permissions.length; i++) {
+                if (grantResults[i] == PackageManager.PERMISSION_GRANTED) {
+                    Toast.makeText(this, permissions[i] + " 권한이 승인됨.", Toast.LENGTH_LONG).show();
+                } else {
+                    Toast.makeText(this, permissions[i] + " 권한이 승인되지 않음.", Toast.LENGTH_LONG).show();
+                }
+            }
         }
     }
 }
