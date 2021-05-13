6814bd86182a86d7438a57bbbf02d52983484d5b
jh675 <kerl6632@gmail.com>
2021-05-13 20:20:04 +0900

    pick

--- app/src/main/java/com/example/hotplego/MainActivity.java
+++ app/src/main/java/com/example/hotplego/MainActivity.java
@@ -1,20 +1,29 @@
 package com.example.hotplego;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class MainActivity extends AppCompatActivity {
+
+    Button button_notice;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        
-        // 조원용
-        // 이주영 1
-        // 박호용
-        // 김진환
-        //313213
+
+        button_notice = findViewById(R.id.button12);
+
+        button_notice.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), PickActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/hotplego/PickActivity.java
@@ -0,0 +1,14 @@
+package com.example.hotplego;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class PickActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.pick_list);
+    }
+}
