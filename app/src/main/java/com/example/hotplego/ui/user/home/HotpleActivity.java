d026561ecb6729ab1d53a403445ac31e449d137b
Park <baallon@g.yju.ac.kr>
2021-06-18 16:05:40 +0900

    call(ACTION_DIAL)내장 수정

--- app/src/main/java/com/example/hotplego/ui/user/home/HotpleActivity.java
+++ app/src/main/java/com/example/hotplego/ui/user/home/HotpleActivity.java
@@ -46,7 +46,9 @@ public class HotpleActivity extends AppCompatActivity {
         binding.call.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(Intent.ACTION_DIAL, Uri.parse(""));
+                Intent intent = new Intent();
+                intent.setAction(Intent.ACTION_DIAL);
+                intent.setData(Uri.parse("tel:01075667311"));
                 startActivity(intent);
             }
         });
